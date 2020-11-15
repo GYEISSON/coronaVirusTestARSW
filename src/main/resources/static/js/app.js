1c4c058b4032312909f883d9d52bc9a4514e8c50
GYEISSON <yeison7f@gmail.com>
2020-11-15 04:43:05 -0500

    agregando el mapa en front

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,54 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+	if (document.querySelectorAll('#map').length > 0)
+	{
+	  if (document.querySelector('html').lang)
+		lang = document.querySelector('html').lang;
+	  else
+		lang = 'en';
+  
+	  var js_file = document.createElement('script');
+	  js_file.type = 'text/javascript';
+	  js_file.src = 'https://maps.googleapis.com/maps/api/js?callback=initMap&signed_in=true&language=' + lang;
+	  document.getElementsByTagName('head')[0].appendChild(js_file);
+	}
+  });
+  
 var app = ( function() {
+	var map;
+	var markers;
+	var bounds;
+
+	function initMap()
+	{
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat:37.566, lng: 126.9784},
+			zoom: 8
+		});
+		fetch('https://raw.githubusercontent.com/jayshields/google-maps-api-template/master/markers.json')
+			.then(function(response){return response.json()})
+			.then(plotMarkers);
+	}
+	function plotMarkers(m)
+	{
+	markers = [];
+	bounds = new google.maps.LatLngBounds();
+
+	m.forEach(function (marker) {
+		var position = new google.maps.LatLng(marker.location.latitude, marker.location.longitude);
+
+		markers.push(
+		new google.maps.Marker({
+			position: position,
+			map: map,
+			animation: google.maps.Animation.DROP
+		})
+		);
+
+		bounds.extend(position);
+	});
+
+	map.fitBounds(bounds);
+	}
 
 	function topCountries() {
 
@@ -55,6 +104,8 @@ var app = ( function() {
 	}
 
 	return {
+		initMap:initMap,
+		plotMarkers:plotMarkers,
 		topCountries:topCountries,
 		allCountries:allCountries,
 		mostrarCountry:mostrarCountry,
